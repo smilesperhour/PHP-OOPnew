f91e859dd9d0e070986407fdd64b575abd2cd4ed
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-01-08 12:27:55 -0800

    lecture 3  *new*

--- run.php
+++ run.php
@@ -14,6 +14,40 @@ var_dump(is_double(123));
 
 var_dump(is_null(false));
 
+
+//----------------------->
+
+
+echo “It is a string”;
+
+else
+
+echo ‘It is not a string’;
+
+var_dump(is_resource(‘frjnrr’));
+
+var_dump(is_bool(‘8008’));
+
+var_dump(is_object(123));
+
+var_dump(is_array(argg));
+
+//-------------------------->
+
+echo “It is a string”;
+
+else
+
+echo ‘It is not a string’;
+
+var_dump(is_string(‘car’));
+
+var_dump(is_string(‘motorcycle’));
+
+var_dump(is_string(biyclycle));
+
+var_dump(is_string(skateboard));
+
 //in your example it is outputting. is it a string bool(true) bool(true bool(false) bool(false)
 // i think it is outputting that because the "type check" is is_string so if it is a string than it will output
 // true if not, false. 
