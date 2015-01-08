3ad34881ebb290c8112fdac743d344a9ceb2f3d6
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-01-07 20:46:32 -0800

    commit

---
+++ notes.php
@@ -0,0 +1,41 @@
+<?php
+// example 1 
+class wolf; 
+class cat;
+class pig;
+
+// example 2 
+$wolf1 = wolf();
+$bike1 = bike();
+$cat1 = cat();
+
+// example3
+class bike{
+	public $engine = "1000cc";
+	public $wheels = "winter";
+	public $handlebar = "short"; 
+	public $muffler = "straight"; 
+
+}
+
+class dog{
+	public $breed = "german";
+	public $legs = "short";
+	public $head = "small"; 
+	public $nose = "big"; 
+
+} 
+
+class car{
+	public $engine = "300hp";
+	public $wheels = "winter";
+	public $steering = "good"; 
+	public $muffler = "straight"; 
+
+} 
+
+//example 4
+$bike1 = new Dog();
+print $dog1->name;
+
+?>
