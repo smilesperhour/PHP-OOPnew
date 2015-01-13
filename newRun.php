4865aa58dbf11140d17c6fd6a3b3a07ad6d7edfd
jonathan benelisha <jonathanbenelisha@gmail.com>
2015-01-13 11:42:12 -0800

    hw for dan. function

---
+++ newRun.php
@@ -0,0 +1,35 @@
+ <?php
+        // The code below creates the class
+        class motorcycle {
+            // Creating some properties (variables tied to an object)
+            public $brand = "honda";
+            public $model = "cbr";
+            public $engine = "1000cc";
+            public $type = "superSport";
+            public $condition = "new";
+            
+            // Assigning the values
+            public function __construct($brand, $model, $engine, $type, $condition) {
+              $this->model = $model;
+              $this->engine = $engine;
+              $this->type = $type;
+              $this->condition = $condition;
+            }
+            
+            // Creating a method (function tied to an object)
+            public function motto() {
+              return "Hello, my bike is a " . $this->model . " " . $this->engine . ". is the engine type";
+            }
+
+            public function car() {
+               return "the bike is in " . $this->condition . " " . " it is a " . $this->type . ".";
+            }
+          }
+
+              // Creating a new person called "boring 12345", who is 12345 years old ;-)
+        $me = new motorcycle('honda', 'CBR', '1000cc', 'new');
+        
+        // Printing out, what the greet method returns
+        echo $me->motto(); 
+        echo $me->car(); 
+        ?>
